d279214062c5f212c6b821abd17d460a299cc502
noiissyboy <vigneshwaran.india@gmail.com>
2023-05-10 15:43:28 +0530

    removed demo

---
+++ public/data/news/index.ts
@@ -0,0 +1,9 @@
+export {  MEDIA_NEWS } from './media';
+
+export interface Hindu {
+    column: string;
+    headline: string;
+    place_date: string;
+    link: string;
+    logo: string;
+}
---
+++ public/data/news/media.ts
@@ -0,0 +1,11 @@
+import { type Hindu } from ".";
+
+export const MEDIA_NEWS: Hindu[] = [
+    {
+        column: "HOME / NEWS / INDIA / KARNATAKA",
+        headline: "Interaction between specific enzyme and protein channel plays crucial role in mitochondrial function: IISc study",
+        place_date: "May 09, 2023 10:14 pm | Updated 10:14 pm IST - Bengaluru",
+        link: "https://www.thehindu.com/news/national/karnataka/interaction-between-specific-enzyme-and-protein-channel-plays-crucial-role-in-mitochondrial-function-iisc-study/article66831027.ece",
+        logo: "/logos/h-circle-black-white-new.svg"
+    }
+]
