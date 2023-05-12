57ed305cd5a97a1adfa50f1e5b9074da67cf36a7
noiissyboy <vigneshwaran.india@gmail.com>
2023-05-12 16:42:01 +0530

    notification for dark mode

--- src/routes/phd-vacancies/index.tsx
+++ src/routes/phd-vacancies/index.tsx
@@ -10,32 +10,32 @@ export default component$(() => {
     return (
         <div class="max-w-[100vw] flex flex-col gap-y-2 spacing light-mode dark:dark-mode border-light dark:border-dark see-through dark:see-through">
             <div class="flex flex-col  border-light dark:border-dark spacing light-mode dark:darker-dark">
-                <h2 class=" spacing  text-2xl font-semibold text-cyan-800">{notice[0].title}</h2>
+                <h2 class=" spacing  text-2xl font-semibold text-cyan-800 dark:text-cyan-100">{notice[0].title}</h2>
                 <div class="spacing">
                     <div class="dark:light-mode dark-mode">
-                        <h2 class=" text-lg font-semibold text-cyan-100 spacing text-center">{notice[0].content.heading}</h2>
+                        <h2 class=" text-lg font-semibold text-cyan-100 dark:text-cyan-800 spacing text-center">{notice[0].content.heading}</h2>
                     </div>
 
                     {
                         notice[0].content.topics.map((topic, index) => (
                             <div key={index}>
-                                <h2 class=" text-lg font-semibold text-cyan-800">{topic.sub_heading}</h2>
-                                <span class="spacing">{topic.content.map((text, index) => (
+                                <h2 class=" text-lg font-semibold text-cyan-800 dark:text-cyan-100 spacing">{topic.sub_heading}</h2>
+                                <span >{topic.content.map((text, index) => (
                                     <p key={index} class=" text-justify">{text}</p>)
                                 )}</span>
                             </div>
                         ))
                     }
                     <h3 class="spacing text-md text-blue-600">{notice[0].content.contact}</h3>
-                    <h3 class=" text-lg font-semibold text-cyan-800">{notice[0].content.focus}</h3>
+                    <h3 class=" text-lg font-semibold text-cyan-800 dark:text-cyan-100 spacing">{notice[0].content.focus}</h3>
                     <h3 class=" text-md  ">{notice[0].content.focus_detail}</h3>
 
 
                 </div>
                 <div class="dark:light-mode dark-mode">
-                    <h3 class=" text-lg font-semibold text-cyan-100 spacing">{notice[0].vacancy}</h3>
+                    <h3 class=" text-lg font-semibold text-cyan-100 dark:text-cyan-800 spacing">{notice[0].vacancy}</h3>
                 </div>
-                <h2 class="text-center text-lg font-semibold text-cyan-800 spacing">Publications:</h2>
+                <h2 class="text-center text-lg font-semibold text-cyan-800 dark:text-cyan-100 spacing">Publications:</h2>
                 
                 <ul class="list-decimal indent-5 spacing">
                     {
