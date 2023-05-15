80f261340fab5fed21746a34f9acc5f0640fee86
noiissyboy <vigneshwaran.india@gmail.com>
2023-05-15 18:17:08 +0530

    eleminated local style for flowind shadow

--- src/components/notice/notice.tsx
+++ src/components/notice/notice.tsx
@@ -1,33 +1,15 @@
-import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 import { Link } from "@builder.io/qwik-city";
-
 
 export const Notice = component$(() => {
 
-    useStylesScoped$(`
-    .card {
-        background:var(--clr-2);
-        position:relative;
-    }
-    .card::before,
-    .card::after {
-        position:absolute;
-        inset:-.75rem;
-        z-index: -1;
-        background:conic-gradient(from var(--gradient-angle),var(--clr-3), var(--clr-4), var(--clr-5),var(--clr-4),var(--clr-3));
-        border-radius:inherit;
-        animation: rotation 5s linear infinite;
-    }
-    .card::after {
-        filter:blur(.75rem);
-    }
-    `)
     return <div class="h-150 w-full rounded-lg see-through dark:see-through light-mode dark:dark-mode border-light dark:border-dark spacing ">
         <h2 class="text-center spacing items-center text-xl font-bold">Notification</h2>
         <div class="spacing  light-mode dark:darker-dark  border-light dark:border-dark ">
-            <div class="card text-white after:absolute rounded-lg">
-              <Link href="phd-vacancies">PhD position availabe</Link> 
+            <div class={`card relative bg-clr2 rounded-lg text-white after:absolute after:-inset-[.75rem] after:blur-[.75rem] after:-z-10 after:rounded-lg after:bg-gradient-conic after:animate-move-around`}>
+                <Link href="phd-vacancies">PhD position availabe</Link>
             </div>
         </div>
     </div>
 });
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
-     
+      backgroundImage: {
+        'gradient-conic': 'conic-gradient(from var(--gradient-angle), theme("colors.clr3"), theme("colors.clr4"), theme("colors.clr5"), theme("colors.clr4"), theme("colors.clr3"))',
+      },
+      colors: {
+        'clr1': '#212534',
+        'clr2': '#191c29',
+        'clr3': '#5ddcff',
+        'clr4': '#3c67e3',
+        'clr5': '#4e00c2',
+      },
       keyframes: {
         slide: {
           '0%': { transform: 'translateX(75%)' },
@@ -18,15 +27,33 @@ module.exports = {
         out: {
           '0%': {transform: 'translateX(0%)'},
           '100%': {transform: 'translateX(100%)'}
-        }   
+        },
+        rotation: {
+          '0%': { '--gradient-angle': '0deg' },
+          '100%': { '--gradient-angle': '360deg' },
+        },
+
       },
       animation: {
         'slide-in': 'in 0.5s ease-in',
         'slide-out': 'out 0.5s ease-out',
+        'move-around':'rotation 5s linear infinite'
       }
     },
   },
-  plugins: [],
+  plugins: [
+    // plugin(function ({ addComponents }) {
+    //   const components = {
+    //     '@property --gradient-angle': {
+    //       syntax: '<angle>',
+    //       'initial-value': '0deg',
+    //       inherits: false,
+    //     },
+    //   };
+
+    //   addComponents(components);
+    // }),
+  ],
   darkMode: 'class'
 };
 
