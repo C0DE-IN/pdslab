13e5867ab489f46911e092445324e3c03d39ce45
noiissyboy <vigneshwaran.india@gmail.com>
2023-06-27 18:17:58 +0530

    spotlight

--- src/components/notice/notice.tsx
+++ src/components/notice/notice.tsx
@@ -7,7 +7,7 @@ export const Notice = component$(() => {
         <h2 class="text-center spacing items-center text-xl font-bold">Notification</h2>
         <div class="spacing  light-mode dark:darker-dark  border-light dark:border-dark ">
             <div class={`card relative bg-clr2 rounded-lg text-white after:absolute after:-inset-[.75rem] after:blur-[.75rem] after:-z-10 after:rounded-lg after:bg-gradient-conic after:animate-move-around`}>
-                <Link href="phd-vacancies">PhD position availabe</Link>
+                <Link href="https://www.cell.com/trends/plant-science/fulltext/S1360-1385(23)00193-0">SPOTLIGHT | Trends in Plant Science</Link>
             </div>
         </div>
     </div>
